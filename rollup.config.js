101c0569445932b992c32e3aeaefae1c3bd6e1f9
Hiroaki Yutani <yutani.ini@gmail.com>
2017-03-07 21:41:09 +0900

    use JavaScript

---
+++ rollup.config.js
@@ -0,0 +1,16 @@
+import nodeResolve from "rollup-plugin-node-resolve";
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'src/main.js',
+  plugins: [
+    nodeResolve({
+      jsnext: true,
+      main: true
+    }),
+    babel({
+      exclude: 'node_modules/**'
+    })
+  ],
+  dest: 'docs/bundle.js'
+};
---
+++ src/main.js
@@ -0,0 +1,19 @@
+// original code: https://bl.ocks.org/mbostock/3883245
+
+import * as d3_selection from "d3-selection";
+import * as d3_time_format from "d3-time-format";
+import * as d3_scale from "d3-scale";
+
+var svg = d3_selection.select("svg"),
+    margin = {top: 20, right: 20, bottom: 30, left: 50},
+    width = +svg.attr("width") - margin.left - margin.right,
+    height = +svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + ", " + margin.top + ")");
+
+var parseTime = d3_time_format.timeParse("%d-%b-%y");
+
+var x = d3_scale.ScaleTime()
+    .rangeRound([0, width]);
+
+var y = d3_scale.ScaleLinear()
+    .rangeRound([height, 0]);
